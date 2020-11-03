4250eaee692c8ab344205c7f9ec76e19268fbc1d
anakonda <air.lukasz89@gmail.com>
2020-11-03 17:52:46 +0100

    snake

--- Display.js
+++ Display.js
@@ -4,23 +4,29 @@ class Display {
     this.width = width;
     this.pixels = [];
 
-    this.init();
-  }
-
-  init() {
-    for (let x = 0; x < this.width; x++) {
-      for (let y = 0; y < this.height; y++) {
-        let pixel = new Pixel(x, y, "black");
-        this.pixels.push(pixel);
+    this.init = () => {
+      for (let x = 0; x < this.width; x++) {
+        for (let y = 0; y < this.height; y++) {
+          let pixel = new Pixel(x, y, "black");
+          this.pixels.push(pixel);
+        }
       }
-    }
+    };
+
+    this.getPixel = (x, y) => {
+      return this.pixels.find(function (p) {
+        return p.x == x && p.y == y;
+      });
+    };
+
+    this.init();
+    //TODO tymczasowy
+    // this.changeColor(4, 9, "green");
   }
 
   changeColor(x, y, color) {
-    var index = this.pixels.findIndex(function (p) {
-      return p.x == x && p.y == y;
-    });
-    this.pixels[index].color = color;
+    let pixel = this.getPixel(x, y);
+    pixel.color = color;
   }
 
   render(element) {
@@ -35,8 +41,12 @@ class Display {
         const row = document.createElement("div");
         row.className = "row";
 
+        let pixel = this.getPixel(x, y);
+        row.style.backgroundColor = pixel.color;
+
         column.appendChild(row);
       }
+
       container.appendChild(column);
     }
     element.innerHTML = container.outerHTML;
