ed3d2b917d19252c55b9d3ce11e2700bf350dbfc
anakonda <air.lukasz89@gmail.com>
2020-11-09 22:05:52 +0100

    Created and inplemenated ClassInputManager

--- Display.js
+++ Display.js
@@ -1,54 +1,56 @@
 class Display {
   constructor(height, width) {
-    this.height = height;
-    this.width = width;
-    this.pixels = [];
+    let _height = height;
+    let _width = width;
+    let _pixels = [];
 
-    this.init = () => {
-      for (let x = 0; x < this.width; x++) {
-        for (let y = 0; y < this.height; y++) {
+    let _init = () => {
+      for (let x = 0; x < _width; x++) {
+        for (let y = 0; y < _height; y++) {
           let pixel = new Pixel(x, y, "black");
-          this.pixels.push(pixel);
+          _pixels.push(pixel);
         }
       }
     };
 
-    this.getPixel = (x, y) => {
-      return this.pixels.find(function (p) {
+    let _getPixel = (x, y) => {
+      return _pixels.find(function (p) {
         return p.x == x && p.y == y;
       });
     };
 
-    this.init();
+    _init();
     //TODO tymczasowy
     // this.changeColor(4, 9, "green");
-  }
-
-  changeColor(x, y, color) {
-    let pixel = this.getPixel(x, y);
-    pixel.color = color;
-  }
-
-  render(element) {
-    let container = document.createElement("div");
-    container.className = "container";
-
-    for (let x = 0; x < this.width; x++) {
-      const column = document.createElement("div");
-      column.className = "column";
-
-      for (let y = 0; y < this.height; y++) {
-        const row = document.createElement("div");
-        row.className = "row";
-
-        let pixel = this.getPixel(x, y);
-        row.style.backgroundColor = pixel.color;
-
-        column.appendChild(row);
-      }
-
-      container.appendChild(column);
+    this.changeColor = (x, y, color) => {
+      let pixel = _getPixel(x, y);
+      pixel.color = color;
     }
-    element.innerHTML = container.outerHTML;
+
+    this.render = (element) => {
+      let container = document.createElement("div");
+      container.className = "container";
+
+      for (let x = 0; x < _width; x++) {
+        const column = document.createElement("div");
+        column.className = "column";
+
+        for (let y = 0; y < _height; y++) {
+          const row = document.createElement("div");
+          row.className = "row";
+
+          let pixel = _getPixel(x, y);
+          row.style.backgroundColor = pixel.color;
+
+          column.appendChild(row);
+        }
+
+        container.appendChild(column);
+      }
+      element.innerHTML = container.outerHTML;
+    }
   }
+
+
+
 }
