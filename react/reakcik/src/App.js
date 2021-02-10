078fff2a3a159b73de36e0a5e9ae18f320274ee8
anakonda <air.lukasz89@gmail.com>
2021-02-10 21:56:37 +0100

    ysztr

--- react/reakcik/src/App.js
+++ react/reakcik/src/App.js
@@ -3,10 +3,33 @@ import './App.css';
 import React, { Component } from 'react';
 
 class App extends React.Component {
+  componentDidMount() {
+    document.title = "dfsdfsdaaaafsd"
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { value: logo };
+
+    this.handleChange = this.handleChange.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    //alert("!");
+
+    this.setState({ value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrgsl5tFVM3bwZDBCUo_tGF70oli7ejLNGmw&usqp=CAU" });
+  }
+
+  handleSubmit(event) {
+    //alert('A name was submitted: ' + this.state.value);
+    //event.preventDefault();
+  }
+
   render() {
-    return <div className="App">
+    return <div className="App" onClick={this.handleChange}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"  />
+        <img src={this.state.value} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
